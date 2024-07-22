fabcffad3643879768f69d76e045e1050136434e
Natalie Poche <nataliealena03@gmail.com>
2024-07-22 02:17:45 -0400

    Added main template, graph, and instructions.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "graph.h"
+using namespace std;
+
+int main(){
+    int no_of_lines, power_iterations;
+    string from, to;
+    cin >> no_of_lines;
+    cin >> power_iterations;
+    for(int i = 0; i < no_of_lines; i++){
+        cin >> from;
+        cin>> to;
+        // Do something
+    }
+}
